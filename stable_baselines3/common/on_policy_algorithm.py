4d8d5b59f305ef13360fda79e194df306971e6c1
Andrew <48459485+AndrewPaulChester@users.noreply.github.com>
2021-03-16 13:52:27 +1100

    Fixed epsiode timeout for n-steps=1

--- stable_baselines3/common/on_policy_algorithm.py
+++ stable_baselines3/common/on_policy_algorithm.py
@@ -184,11 +184,14 @@ class OnPolicyAlgorithm(BaseAlgorithm):
             self._last_dones = dones
 
         with th.no_grad():
+            #Fixing episode timeout - only correct for num-steps = 1
+            true_obs = [[x['s_true']] for x in infos]
+            true_done = np.array([x['d_true'] for x in infos],dtype=np.bool)
             # Compute value for the last timestep
             #obs_tensor = th.as_tensor(new_obs).to(self.device)
-            _, values, _ = self.policy.forward(new_obs)
+            _, values, _ = self.policy.forward(true_obs)
 
-        rollout_buffer.compute_returns_and_advantage(last_values=values, dones=dones)
+        rollout_buffer.compute_returns_and_advantage(last_values=values, dones=true_done)
 
         callback.on_rollout_end()
 
